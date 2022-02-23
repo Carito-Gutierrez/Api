6f5b35c778d124e1256211d077281c3320a9e05e
Carito-Gutierrez <sankrito0417@gmail.com>
2022-02-23 18:00:47 -0500

    Agrega carpeta de practica de angular

---
+++ bit-shop-front/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+
+
+// enrutamiento de componentes
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: '**', component: NotFoundComponent},
+  {path: 'footer', component: FooterComponent}
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
